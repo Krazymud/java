ff193f13d56d49345ae800eec9abab42943bcfa6
Eadric <cy735322470@gmail.com>
2018-04-22 16:37:47 +0800

    ch

--- store/GridWorldCode/projects/JumperTest.java
+++ store/GridWorldCode/projects/JumperTest.java
@@ -5,8 +5,11 @@ import info.gridworld.grid.Location;
 
 public class JumperTest extends TestCase{
 
-	public void testa(){
-		ActorWorld world = new ActorWorld();
+        /**
+        *  Test the move when there's a rock two steps ahead of the jumper.
+        */
+        public void testa(){
+                ActorWorld world = new ActorWorld();
         Jumper alice = new Jumper();
         Rock rock = new Rock();
         world.add(new Location(7, 2), alice);
@@ -14,19 +17,24 @@ public class JumperTest extends TestCase{
         alice.act();
         Location loc = new Location(6, 2);
         assertEquals(loc, alice.getLocation());
-	}
+        }
 
-	public void testb(){
-		ActorWorld world = new ActorWorld();
+        /**
+        *  Test the move when there's nothing two steps ahead of the jumper.
+        */
+        public void testb(){
+                ActorWorld world = new ActorWorld();
         Jumper alice = new Jumper();
         world.add(new Location(1, 2), alice);
         alice.act();
         Location loc = new Location(0, 2);
         assertEquals(loc, alice.getLocation());
-	}
-
-	public void testc(){
-		ActorWorld world = new ActorWorld();
+        }
+        /**
+        *  Test the move when the jumper is facing the edge
+        */
+        public void testc(){
+                ActorWorld world = new ActorWorld();
         Jumper alice = new Jumper();
         world.add(new Location(0, 2), alice);
         alice.act();
@@ -36,33 +44,29 @@ public class JumperTest extends TestCase{
         alice.act();
         Location loc = new Location(0, 4);
         assertEquals(loc, alice.getLocation());
-	}
-
-	public void testd(){
-		ActorWorld world = new ActorWorld();
+        }
+        /**
+        *  Test the move when there's a jumper two steps ahead of the jumper.
+        *  Two cases: contested or not contested.
+        */
+        public void testd(){
+                ActorWorld world = new ActorWorld();
         Jumper alice = new Jumper();
         Jumper bob = new Jumper();
         world.add(new Location(5, 5), alice);
         world.add(new Location(3, 5), bob);
+        world.add(new Location(7, 7), tom);
+        world.add(new Location(9, 9), pat);
+        tom.setDirection(90);
         bob.setDirection(90);
         world.step();
         Location loc1 = new Location(3, 5);
         Location loc2 = new Location(3, 7);
+        Location loc3 = new Location(7, 9);
+        Location loc4 = new Location(8, 9);
         assertEquals(loc1, alice.getLocation());
         assertEquals(loc2, bob.getLocation());
-	}
-
-	public void teste(){
-		ActorWorld world = new ActorWorld();
-        Jumper alice = new Jumper();
-        Jumper bob = new Jumper();
-        world.add(new Location(5, 5), alice);
-        world.add(new Location(3, 3), bob);
-        bob.setDirection(90);
-        world.step();
-        Location loc1 = new Location(4, 5);
-        Location loc2 = new Location(3, 5);
-        assertEquals(loc1, alice.getLocation());
-        assertEquals(loc2, bob.getLocation());
-	}
+        assertEquals(loc3, tom.getLocation());
+        assertEquals(loc4, pat.getLocation());
+        }
 }
